9366ed4c0477c6b467596449346aaa8c55057806
211272183 <211272183@mycput.ac.za>
2022-02-08 11:12:21 +0200

    second commit

--- src/za/ac/cput/third/loza.java
+++ src/za/ac/cput/third/loza.java
@@ -3,5 +3,6 @@ package za.ac.cput.third;
 public class loza {
     public static void main(String[] args) {
         System.out.println("hello hi");
+        System.out.println("this is my playground");
     }
 }
